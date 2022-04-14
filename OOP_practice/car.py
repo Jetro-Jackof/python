bf28d6e2b16470b79c886d1321a58dafe377a8b0
Jetro Oropeza <jetrojackof@hotmail.com>
2022-04-13 22:14:17 -0500

    oop car practice and new web scraping practice

--- OOP_practice/car.py
+++ OOP_practice/car.py
@@ -15,6 +15,36 @@ class Car():
     def read_odometer(self):
         print(f'This car has {str(self.odometer_reading)} miles on it.')
 
+    #update the value of the odometer
+    def update_odometer(self,mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back the odometer")
+    
+    #increase odometer value
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
 new_car = Car('audi', 'a4', 2016)
 print(new_car.get_descriptive_name())
 new_car.read_odometer()
+print('\n')
+
+#test update odometer
+new_car.update_odometer(23)
+new_car.read_odometer()
+print('\n')
+
+#second test
+used_car = Car('subaru', 'outback', 2013)
+print(used_car.get_descriptive_name())
+used_car.update_odometer(23500)
+used_car.read_odometer()
+
+used_car.increment_odometer(100)
+used_car.read_odometer()
+
+
+
+
